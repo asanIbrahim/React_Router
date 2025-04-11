30ce904ccf3452eb964ca73424eecb89a5a6a966
asanIbrahim <asanib98@gmail.com>
2025-04-11 19:32:28 +0530

    add useReducer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,44 @@
+import { useReducer } from "react";
+import Home from "../src/components/Home";
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "increment":
+      return { count: state.count + 1 };
+    case "decrement":
+      if (state.count > 0) {
+        return { count: state.count - 1 };
+      }
+      break;
+    default:
+      return state;
+  }
+}
+
 function App() {
-  return <div className="App"></div>;
+  // const [count, Setcount] = useState(0);
+  const [state, disPatch] = useReducer(reducer, { count: 0 });
+
+  const increaseCount = () => {
+    disPatch({ type: "increment" });
+  };
+
+  const decrementcount = () => {
+    disPatch({ type: "decrement" });
+  };
+
+  return (
+    <>
+      <div>
+        <h1>
+          <button onClick={increaseCount}>+</button>
+          <button onClick={decrementcount}>-</button>
+          {state.count}
+          <Home />
+        </h1>
+      </div>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/User.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Users from "./Users";
+
+const User = () => {
+  return (
+    <div>
+      UserHome
+      <Users />
+    </div>
+  );
+};
+export default User;
