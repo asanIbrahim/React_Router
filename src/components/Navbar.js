f44585c2b53918afc904abb2643022f33b278fa5
asanIbrahim <asanib98@gmail.com>
2025-04-14 13:45:10 +0530

    add route

---
+++ src/components/Navbar.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Navbar.css";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <h1>Welcome</h1>
+      <ul className="nav-left">
+        <Link to="/" className="nav-link">
+          Home
+        </Link>
+        <Link to="/user" className="nav-link">
+          User
+        </Link>
+        <Link to="/about" className="nav-link">
+          About
+        </Link>
+        <Link to="/profile" className="nav-link">
+          profile
+        </Link>
+        {/* <Link to="/instagram/userName" className="nav-link">
+          Instagram
+        </Link> */}
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
